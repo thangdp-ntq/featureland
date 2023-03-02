173170b9a85349abb0230ec656efea6225367d4e
thangdp-ntq <thang.duong@ntq-solution.com.vn>
2023-03-02 11:41:25 +0700

    init pr

--- src/users/dto/search-user.dto.ts
+++
@@ -1,8 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { ApiProperty } from '@nestjs/swagger';
-import { SearchDto } from '../../common/search.dto';
-
-export class SearchUserDto extends PartialType(SearchDto) {
-  @ApiProperty({ type: String, example: 'address' })
-  address: string;
-}
--- src/webhook/webhook.module.ts
+++ src/webhook/webhook.module.ts
@@ -38,13 +38,7 @@ import { EventsGateway } from '../socket/socket.gateway';
 import { UploadService } from '../upload/upload.service';
 import { WebhookController } from './webhook.controller';
 import { WebhookService } from './webhook.service';
-import { FNftPoolService } from '../f-nft-pool/f-nft-pool.service';
-import { FNFTPoolValidator } from '../f-nft-pool/f-nft-pool.validator';
-import { TieringStructureService } from '../tiering-structure/tiering-structure.service';
-import { TieringPoolService } from '../tiering-pool/tiering-pool.service';
-import { RewardPoolService } from '../reward-pool/reward-pool.service';
 import { ValidateAddressWalletService } from '~/common/services/validate-wallet-address.service';
-import { ClaimRewardService } from '../claim-reward/claim-reward.service';
 import {
   ClaimRewardHistory,
   ClaimRewardHistorySchema,
@@ -59,9 +53,6 @@ import { CommonService } from '~/common-service/common.service';
 import { Nonce, NonceSchema } from '~/schemas/nonce.schema';
 import { PriceToken, PriceTokenSchema } from '~/schemas/price-token.schema';
 import { CommonModule } from '~/common-service/common.module';
-import { TimeSetting, TimeSettingSchema } from '~/schemas/time-setting.schema';
-import { NFTLog, NFTLogSchema } from '~/schemas/nft-log.schema';
-import { SystemWalletService } from '~/system-wallet/system-wallet.service';
 import {
   SystemWallet,
   SystemWalletSchema,
@@ -72,32 +63,7 @@ import { GasLimit, GasLimitSchema } from '~/schemas/gas-limit.schema';
 @Module({
   imports: [
     WinstonModule,
-    MongooseModule.forFeature([
-      { name: NFT.name, schema: NFTSchema },
-      { name: BatchLabel.name, schema: BatchLabelSchema },
-      { name: User.name, schema: UserSchema },
-      { name: FNFTPool.name, schema: FNFTPoolSchema },
-      { name: Timeline.name, schema: TimelineSchema },
-      { name: Serial.name, schema: SerialSchema },
-      { name: Path.name, schema: PathSchema },
-      { name: TieringStructure.name, schema: TieringStructureSchema },
-      { name: HistoryStaking.name, schema: HistoryStakingSchema },
-      { name: RewardPool.name, schema: RewardPoolSchema },
-      { name: TieringPool.name, schema: TieringPoolSchema },
-      { name: RewardPool.name, schema: RewardPoolSchema },
-      { name: PurchaseFNFT.name, schema: PurchaseFNFTSchema },
-      { name: ClaimRewardHistory.name, schema: ClaimRewardHistorySchema },
-      { name: Signer.name, schema: SignerSchema },
-      { name: MetaDataFields.name, schema: MetaDataFieldSchema },
-      { name: IdPool.name, schema: IdPoolSchema },
-      { name: Nonce.name, schema: NonceSchema },
-      { name: PriceToken.name, schema: PriceTokenSchema },
-      { name: TimeSetting.name, schema: TimeSettingSchema },
-      { name: NFTLog.name, schema: NFTLogSchema },
-      { name: SystemWallet.name, schema: SystemWalletSchema },
-      { name: SettingWallet.name, schema: SettingWalletSchema },
-      { name: GasLimit.name, schema: GasLimitSchema },
-    ]),
+    MongooseModule.forFeature(),
     CommonModule,
     EmailModule,
   ],
@@ -107,16 +73,9 @@ import { GasLimit, GasLimitSchema } from '~/schemas/gas-limit.schema';
     UploadService,
     NftService,
     EventsGateway,
-    FNftPoolService,
-    FNFTPoolValidator,
-    TieringStructureService,
-    TieringPoolService,
-    RewardPoolService,
     ValidateAddressWalletService,
-    ClaimRewardService,
     SignerService,
     CommonService,
-    SystemWalletService,
   ],
 })
 export class WebhookModule {}
