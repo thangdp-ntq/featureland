88cd47a2e0a5eba3d0d061a30877a1557c54ce09
thang551999 <duongphucthangts@gmail.com>
2023-03-25 23:51:09 +0700

    add

--- src/nft/nft.service.ts
+++ src/nft/nft.service.ts
@@ -174,10 +174,10 @@ export class NftService {
       });
   }
   async TranferNft(data) {
-    const nftLogs = await this.logModel.findOne({
-      transactionHash: data.transactionHash,
-    });
-    if (nftLogs) return true;
+    // const nftLogs = await this.logModel.findOne({
+    //   transactionHash: data.transactionHash,
+    // });
+    // if (nftLogs) return true;
     const nft = await this.nftModel.findOne({
       tokenId: Number(data.metadata.tokenId),
     });
